7248a49f291b2f964bf7dc6324dba49634bc99fb
Haris Naveed <haris.naveed174@gmail.com>
2020-12-25 16:26:39 +0500

    Add normalizer function

--- utils.py
+++ utils.py
@@ -23,11 +23,13 @@ utils.py
 ## Necessary Packages
 import numpy as np
 import tensorflow as tf
+import pandas as pd
+import copy
 import data_loading as dl
 
 
-def train_test_divide (data_x, data_x_hat, data_t, data_t_hat, train_rate = 0.8):
-  """Divide train and test data for both original and synthetic data.
+def train_test_divide(data_x, data_x_hat, data_t, data_t_hat, train_rate=0.8):
+    """Divide train and test data for both original and synthetic data.
   
   Args:
     - data_x: original data
@@ -36,33 +38,33 @@ def train_test_divide (data_x, data_x_hat, data_t, data_t_hat, train_rate = 0.8)
     - data_t_hat: generated time
     - train_rate: ratio of training data from the original data
   """
-  # Divide train/test index (original data)
-  no = len(data_x)
-  idx = np.random.permutation(no)
-  train_idx = idx[:int(no*train_rate)]
-  test_idx = idx[int(no*train_rate):]
-    
-  train_x = [data_x[i] for i in train_idx]
-  test_x = [data_x[i] for i in test_idx]
-  train_t = [data_t[i] for i in train_idx]
-  test_t = [data_t[i] for i in test_idx]      
-    
-  # Divide train/test index (synthetic data)
-  no = len(data_x_hat)
-  idx = np.random.permutation(no)
-  train_idx = idx[:int(no*train_rate)]
-  test_idx = idx[int(no*train_rate):]
-  
-  train_x_hat = [data_x_hat[i] for i in train_idx]
-  test_x_hat = [data_x_hat[i] for i in test_idx]
-  train_t_hat = [data_t_hat[i] for i in train_idx]
-  test_t_hat = [data_t_hat[i] for i in test_idx]
-  
-  return train_x, train_x_hat, test_x, test_x_hat, train_t, train_t_hat, test_t, test_t_hat
+    # Divide train/test index (original data)
+    no = len(data_x)
+    idx = np.random.permutation(no)
+    train_idx = idx[:int(no * train_rate)]
+    test_idx = idx[int(no * train_rate):]
+
+    train_x = [data_x[i] for i in train_idx]
+    test_x = [data_x[i] for i in test_idx]
+    train_t = [data_t[i] for i in train_idx]
+    test_t = [data_t[i] for i in test_idx]
+
+    # Divide train/test index (synthetic data)
+    no = len(data_x_hat)
+    idx = np.random.permutation(no)
+    train_idx = idx[:int(no * train_rate)]
+    test_idx = idx[int(no * train_rate):]
+
+    train_x_hat = [data_x_hat[i] for i in train_idx]
+    test_x_hat = [data_x_hat[i] for i in test_idx]
+    train_t_hat = [data_t_hat[i] for i in train_idx]
+    test_t_hat = [data_t_hat[i] for i in test_idx]
+
+    return train_x, train_x_hat, test_x, test_x_hat, train_t, train_t_hat, test_t, test_t_hat
 
 
-def extract_time (data):
-  """Returns Maximum sequence length and each sequence length.
+def extract_time(data):
+    """Returns Maximum sequence length and each sequence length.
   
   Args:
     - data: original data
@@ -71,17 +73,17 @@ def extract_time (data):
     - time: extracted time information
     - max_seq_len: maximum sequence length
   """
-  time = list()
-  max_seq_len = 0
-  for i in range(len(data)):
-    max_seq_len = max(max_seq_len, len(data[i][:, 0]))
-    time.append(len(data[i][:, 0]))
-    
-  return time, max_seq_len
+    time = list()
+    max_seq_len = 0
+    for i in range(len(data)):
+        max_seq_len = max(max_seq_len, len(data[i][:, 0]))
+        time.append(len(data[i][:, 0]))
+
+    return time, max_seq_len
 
 
 def rnn_cell(module_name, hidden_dim):
-  """Basic RNN Cell.
+    """Basic RNN Cell.
     
   Args:
     - module_name: gru, lstm, or lstmLN
@@ -89,22 +91,22 @@ def rnn_cell(module_name, hidden_dim):
   Returns:
     - rnn_cell: RNN Cell
   """
-  assert module_name in ['gru','lstm','lstmLN']
-  
-  # GRU
-  if (module_name == 'gru'):
-    rnn_cell = tf.nn.rnn_cell.GRUCell(num_units=hidden_dim, activation=tf.nn.tanh)
-  # LSTM
-  elif (module_name == 'lstm'):
-    rnn_cell = tf.contrib.rnn.BasicLSTMCell(num_units=hidden_dim, activation=tf.nn.tanh)
-  # LSTM Layer Normalization
-  elif (module_name == 'lstmLN'):
-    rnn_cell = tf.contrib.rnn.LayerNormBasicLSTMCell(num_units=hidden_dim, activation=tf.nn.tanh)
-  return rnn_cell
+    assert module_name in ['gru', 'lstm', 'lstmLN']
+
+    # GRU
+    if (module_name == 'gru'):
+        rnn_cell = tf.nn.rnn_cell.GRUCell(num_units=hidden_dim, activation=tf.nn.tanh)
+    # LSTM
+    elif (module_name == 'lstm'):
+        rnn_cell = tf.contrib.rnn.BasicLSTMCell(num_units=hidden_dim, activation=tf.nn.tanh)
+    # LSTM Layer Normalization
+    elif (module_name == 'lstmLN'):
+        rnn_cell = tf.contrib.rnn.LayerNormBasicLSTMCell(num_units=hidden_dim, activation=tf.nn.tanh)
+    return rnn_cell
 
 
-def random_generator (batch_size, z_dim, T_mb, max_seq_len):
-  """Random vector generation.
+def random_generator(batch_size, z_dim, T_mb, max_seq_len):
+    """Random vector generation.
   
   Args:
     - batch_size: size of the random vector
@@ -115,17 +117,17 @@ def random_generator (batch_size, z_dim, T_mb, max_seq_len):
   Returns:
     - Z_mb: generated random vector
   """
-  Z_mb = list()
-  for i in range(batch_size):
-    temp = np.zeros([max_seq_len, z_dim])
-    temp_Z = np.random.uniform(0., 1, [T_mb[i], z_dim])
-    temp[:T_mb[i],:] = temp_Z
-    Z_mb.append(temp_Z)
-  return Z_mb
+    Z_mb = list()
+    for i in range(batch_size):
+        temp = np.zeros([max_seq_len, z_dim])
+        temp_Z = np.random.uniform(0., 1, [T_mb[i], z_dim])
+        temp[:T_mb[i], :] = temp_Z
+        Z_mb.append(temp_Z)
+    return Z_mb
 
 
 def batch_generator(data, time, batch_size):
-  """Mini-batch generator.
+    """Mini-batch generator.
   
   Args:
     - data: time-series data
@@ -136,17 +138,24 @@ def batch_generator(data, time, batch_size):
     - X_mb: time-series data in each batch
     - T_mb: time information in each batch
   """
-  no = len(data)
-  idx = np.random.permutation(no)
-  train_idx = idx[:batch_size]     
-            
-  X_mb = list(data[i] for i in train_idx)
-  T_mb = list(time[i] for i in train_idx)
-  
-  return X_mb, T_mb
+    no = len(data)
+    idx = np.random.permutation(no)
+    train_idx = idx[:batch_size]
+
+    X_mb = list(data[i] for i in train_idx)
+    T_mb = list(time[i] for i in train_idx)
+
+    return X_mb, T_mb
 
 
-# ori_data = np.loadtxt('data/tester_2.csv', delimiter = ",",skiprows = 1)
-# ori_data = dl.real_data_loading('tester', 1)
-# t, seq_len = extract_time(ori_data)
-# print(-1)
+def denormalizer(dat_max, dat_min, generated_data, columns):
+    norm_range = dat_max - dat_min
+    denorm_data = pd.DataFrame({})
+    for i in range(0, generated_data.shape[0]):
+        x = copy.deepcopy(generated_data[i])
+        df = pd.DataFrame(x, columns=columns)
+        for j, k in enumerate(columns):
+            df[k] = (df[k] * norm_range[j]) + dat_min[j]
+        denorm_data = denorm_data.append(df)
+    denorm_data = denorm_data.reset_index(drop=True)
+    return denorm_data
